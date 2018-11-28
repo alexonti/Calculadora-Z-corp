53a5048dc63803e8959076d3f94797555c54b099
alexonti <44008399+alexonti@users.noreply.github.com>
2018-11-27 21:50:08 -0300

    calculadora menu terminal
    se trabaja con menu en terminal para la calculadora

---
+++ CalculadoraTerminal.java
@@ -0,0 +1,29 @@
+package menuterminal;
+
+import java.util.Scanner;
+
+public class CalculadoraTerminal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Calculadora Z-Corp \n");
+		System.out.println("Bienvenido \n\n");
+		
+		System.out.println("Selecciona una de las siguientes opciones \n");
+		
+		System.out.println("1 - Operaciones matemáticas \n" + "2 - Operaciones binarias \n" + "3 - Orden de datos \n" + "4 - Promedios \n" + "5 - Acerca \n" + "6 - Salir \n");
+		
+		String[]menu = {
+        		"1",
+        		"2",
+        		"3",
+        		"4",
+        		"Regresar",  };
+		
+		Scanner teclado=new Scanner(System.in);
+		
+	
+	}
+
+}
